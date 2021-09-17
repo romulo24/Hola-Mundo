c0e3dd413051d636ced4ff5ad24d03544329da43
Rómulo Jesús Torres Llivipuma <71521545+romulo24@users.noreply.github.com>
2021-09-17 18:13:15 -0500

    Add files via upload

---
+++ booleanos.py
@@ -0,0 +1,57 @@
+#Operaciones logicas
+
+#Booleano
+#Relacionales
+print(10 >= 9)
+print("otras palabras" != "otras palabras")
+print("test" in "testing")
+a = 'testing'
+print('test' in a)
+
+#EJERCICIO
+a=5
+b=7
+c=8
+
+print(a>b)
+print(a<b)
+print(a+b<c)
+
+
+x= (a + b > c) | (a + b < c)
+y=(a + b > c) & (a + b < c)
+print(x,y)
+
+#Comparaciones
+#Mayor que; < Menor que
+3 > 2
+#True
+
+3 < 2
+#False
+
+#>= Mayor o igual que; <= Menor o igual que
+2 >= 1 + 1
+#True
+
+4 - 2 <= 1
+#False
+
+#== Igual que; != Distinto de
+2 == 1 + 1
+#Tue
+
+6 / 2 != 3
+#False
+
+4 == 3 + 1 > 2
+#True
+
+2 != 1 + 1 > 0
+#False
+
+4 == 3 + 1 > 2
+#True
+
+2 != 1 + 1 > 0
+#False
