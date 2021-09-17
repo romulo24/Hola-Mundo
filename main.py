822dedca28e313dd9ff04b13d58a4e82ef724ae8
Romulo Torres <torresromulo200@gmail.com>
2021-09-17 10:45:42 -0500

    second commit

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 # Este es mi primer programa usando el lenguaje de programacion en Python
-print('Hola mundo')
+primera_variable=' Romulo'
+#Declaracion de variables de variables
+#Impresion de lineas de caracteres + una variable
+print('Hola mundo'+ primera_variable)
