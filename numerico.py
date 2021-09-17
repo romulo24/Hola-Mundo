6d1f3c63bd5b501dded0720600f95e262427644e
Romulo Torres <torresromulo200@gmail.com>
2021-09-17 11:24:32 -0500

    Ajustando el tema de los metodos numericos

--- numerico.py
+++ numerico.py
@@ -1,13 +1,19 @@
+# Programacion con Python
+# Autor: Estudiante Torres LLivipuma Romulo Jesus <rtorresll@est.ups.edu.ec>
+# Universidad Politecnica Salesiana
+
 #Declaracion de variables de tipo numericos
 #Declaracion de la primera variable
 x=10
 #Declaracion de la segunda variable
 y=25
-#Serie de operaciones numericas
+
+#Serie de operaciones numericas 
 s=x+y #Suma
 r=x-y #Resta
 m=x*y #Multiplicacion
 d=x/y #Division
+
 #Impresion de cada una de las opercaiones realizadas
 print(s) #Impresion de la Suma
 print(r) #Impresion de la Resta
@@ -18,3 +24,9 @@ print(d) #Impresion de la Division
 print(x**(2)) #El valor de la variable x elevado al cuadrado
 print(y**(3)) #El valor de la variable y elevado al cubo
 print(s**(2)) #El valor de la suma de x and y elevado al cuadrado
+
+#Metodos numericos para realizar con la Division
+#// Se utiliza doble barra inclinada para obtener el valor entero de una division
+print(x//y)
+#Valor restante
+print(x%y)
