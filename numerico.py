3d4b6ef69778b79f9a5672b9e0ec6ff96385b38e
Romulo Torres <torresromulo200@gmail.com>
2021-09-17 11:13:38 -0500

    Metodos numericos

---
+++ numerico.py
@@ -0,0 +1,20 @@
+#Declaracion de variables de tipo numericos
+#Declaracion de la primera variable
+x=10
+#Declaracion de la segunda variable
+y=25
+#Serie de operaciones numericas
+s=x+y #Suma
+r=x-y #Resta
+m=x*y #Multiplicacion
+d=x/y #Division
+#Impresion de cada una de las opercaiones realizadas
+print(s) #Impresion de la Suma
+print(r) #Impresion de la Resta
+print(m) #Impresion de la Multiplicacion
+print(d) #Impresion de la Division
+
+#Elevar un valor numerico a una Potencia
+print(x**(2)) #El valor de la variable x elevado al cuadrado
+print(y**(3)) #El valor de la variable y elevado al cubo
+print(s**(2)) #El valor de la suma de x and y elevado al cuadrado
